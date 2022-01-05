f166f1f09d7c37927e89dabc8c30ecb3db05485b
Max <shalyapapro@yandex.ru>
2022-01-05 20:33:21 +0300

    create admin models

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from mainapp.models import UserAnswers, UserQuiz, UserQuestion, UserChoice
+
+
+class UserAnswerAdmin(admin.ModelAdmin):
+    list_display = ('answer',)
+
+
+class UserQuizAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+
+class UserQuestionAdmin(admin.ModelAdmin):
+    list_display = ('text', 'quiz',)
+
+
+class UserChoiceAdmin(admin.ModelAdmin):
+    list_display = ('text', 'question', 'is_correct')
+
+
+admin.site.register(UserAnswers, UserAnswerAdmin)
+admin.site.register(UserQuiz, UserQuizAdmin)
+admin.site.register(UserQuestion, UserQuestionAdmin)
+admin.site.register(UserChoice, UserChoiceAdmin)
+
