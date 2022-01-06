a06a8eb00edf6667268b4e4b3b0c35510aae29f6
Max <shalyapapro@yandex.ru>
2022-01-06 15:34:12 +0300

    updated quiz_result_service

--- quiz/services.py
+++ quiz/services.py
@@ -1,11 +1,28 @@
-from .dto import ChoiceDTO, QuestionDTO, QuizDTO, AnswerDTO, AnswersDTO
-from typing import List
+from quiz.dto import QuizDTO, AnswersDTO
 
 
 class QuizResultService():
+
     def __init__(self, quiz_dto: QuizDTO, answers_dto: AnswersDTO):
         self.quiz_dto = quiz_dto
         self.answers_dto = answers_dto
 
     def get_result(self) -> float:
-        # your code here
+        user_answers = []
+        right_answers = []
+        count = 0
+        for question in self.quiz_dto.questions:
+            right_answers.append([])
+            for choice in question.choices:
+                if choice.is_correct == True:
+                    right_answers[count].append(choice.uuid)
+            count = count + 1
+        right_user_answers = len(right_answers)
+        for user_answer in self.answers_dto.answers:
+            user_answers.append(user_answer.choices)
+        for count in range(len(user_answers)):
+            if str(right_answers[count]) != str(user_answers[count]):
+                right_user_answers -= 1
+        score = right_user_answers / len(right_answers)
+        rounded_score = round(score, 2)
+        return rounded_score
