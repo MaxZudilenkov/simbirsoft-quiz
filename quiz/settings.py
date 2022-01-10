e03c834a021459b183098b63195bc179f17f3e54
Max <shalyapapro@yandex.ru>
2022-01-10 12:48:47 +0300

    updated settings

--- quiz/settings.py
+++ quiz/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!cid&z%fwl@$&fr0pgq+$_v8*hc!vdrq902y)(*%(-9wn^uii8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
